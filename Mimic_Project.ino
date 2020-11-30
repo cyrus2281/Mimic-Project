12dad8eab955caf255452cc5e2ff32985c27aa38
Milad Mobini <miladmobini.ca@gmail.com>
2020-11-29 23:49:40 -0500

    added the speaker tone methods

--- Mimic_Project.ino
+++ Mimic_Project.ino
@@ -1,13 +1,40 @@
-
+//Declaring the I/O pins
+#define speakerPin 5 //the pin for the piezo speaker(PMW)
 
 
 
 void setup() {
 
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+
+//tone one ( ..-)
+void speakerToneOne(){
+tone(speakerPin, 5000, 200); //200 milisec @ 5000Hz
+delay(150);
+tone(speakerPin, 5000, 200); //200 milisec @ 5000Hz
+delay(150);
+tone(speakerPin, 10000, 400); //400 milisec @ 10000Hz
+delay(100);  
+}
+//tone two ( .---)
+void speakerToneTwo(){
+tone(speakerPin, 15000, 200); //200 milisec @ 15000Hz
+delay(150);
+tone(speakerPin, 3000, 1500); //1.5 sec @ 3000Hz
+delay(100);  
+}
+//tone three ( .. ..)
+void speakerToneOne(){
+tone(speakerPin, 5000, 200); //200 milisec @ 5000Hz
+delay(100);
+tone(speakerPin, 5000, 200); //200 milisec @ 5000Hz
+delay(750);
+tone(speakerPin, 5000, 200); //200 milisec @ 5000Hz
+delay(100);
+tone(speakerPin, 5000, 200); //200 milisec @ 5000Hz
+delay(100); 
 }
