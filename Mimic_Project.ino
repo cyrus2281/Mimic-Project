678cb70184c477e255ba619dda6f35d2bc9ab3fb
Milad Mobini <miladmobini.ca@gmail.com>
2020-11-30 01:08:19 -0500

    implementing the learning and mimicing idea

--- Mimic_Project.ino
+++ Mimic_Project.ino
@@ -1,27 +1,55 @@
 //Declaring the I/O pins
 #define speakerPin 5 //the pin for the piezo speaker(PMW)
+#define ledRedPin 6     //RGB led pins (red)
+#define ledGreenPin 7   //RGB led pins (green)
+#define ledBluePin 8   //RGB led pins (blue)
 
 
-//The arm's mode {Manual mode, Learning mode => auto mode}
-volatile boolean mode = false;
+
+//The arm's mode {Manual mode, Learning mode, auto mode}
+volatile boolean modeMan = true;    //this switch mode between manual and learning
+boolean modeAuto = false;         //this switch mode between learning and auto
+
+
+
+
+
 
 void setup() {
   Serial.begin(9600); //Starting serial communications
   Serial.println("Starting...");
   attachInterrupt(0, ISR0, RISING); //attaching an interrupt for changing mode
+  speakerToneOne(); //indicating the start of program
+
 }
 
 void loop() {
-//Manual Mode
+  //Manual Mode
+  if (modeMan) {
+    speakerToneThree(); //indicating change in mode
+    rgbLed('r'); //color coding the mode, Manual red
+
+
+  }
+  else {
+    //Automatic Mode
+    if (modeAuto) {
+      speakerToneThree(); //indicating change in mode
+      rgbLed('b'); //color coding the mode, auto blue
+
+    }
+    //Learning Mode
+    else {
+      speakerToneThree(); //indicating change in mode
+      rgbLed('g'); //color coding the mode, learning green
+
+      speakerToneTwo(); //speaker indicating the learning is over
+      modeAuto = true;
+    }
+  }
 
 
 
-
-//Learning Mode
-
-
-
-//Automatic Mode
 
 
 
@@ -35,18 +63,37 @@ void loop() {
 //intterupt Zero (ISR0)
 void ISR0() {
   detachInterrupt(0); //turning off the interrupt
-  if(!mode){
-    mode=true;           //setting the mode from manual to learning
+  if (modeMan) {
+    modeMan = false;         //setting the mode from manual to learning
     Serial.println("Set mode to learning");
-  }else{
-    mode= false;        //set the mode from learning to manual
-    Serial.println("Set mode to manual");  
+  } else {
+    modeMan = true;       //set the mode from learning to manual
+    modeAuto = false;     //set the mode auto off
+    Serial.println("Set mode to manual");
   }
   delay(100);
   attachInterrupt(0, ISR0, RISING);
 }
-
-
+//a method for the rgb led color settings
+void rgbLed(char color) {
+  digitalWrite(ledRedPin, LOW);
+  digitalWrite(ledGreenPin, LOW);
+  digitalWrite(ledBluePin, LOW);
+  switch (color) {
+    case 'r':
+      Serial.println("RGB LED color set to red");
+      digitalWrite(ledRedPin, HIGH);
+      break;
+    case 'g':
+      Serial.println("RGB LED color set to green");
+      digitalWrite(ledGreenPin, HIGH);
+      break;
+    case 'b':
+      Serial.println("RGB LED color set to blue");
+      digitalWrite(ledBluePin, HIGH);
+      break;
+  }
+}
 //tone one ( ..-)
 void speakerToneOne() {
   tone(speakerPin, 5000, 200); //200 milisec @ 5000Hz
