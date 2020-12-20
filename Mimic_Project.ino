66afefa409a0c4d045ec2cb6c0bdca1f214a5641
Milad Mobini <miladmobini.ca@gmail.com>
2020-12-19 23:46:19 -0500

    upadted the code base on the mpu library, fully functional, not very accurate though, fututre works will be implied to the claw. Congrats to myself for making it this far

--- Mimic_Project.ino
+++ Mimic_Project.ino
@@ -1,6 +1,37 @@
+
+/* ============================================
+  I2Cdev device library code is placed under the MIT license
+  Copyright (c) 2012 Jeff Rowberg
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+  ===============================================
+*/
+
+
 //libraries
 #include <Servo.h>
 #include <Wire.h>
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+
+
+
 
 //Declaring the I/O pins
 #define speakerPin  4     //the pin for the piezo speaker(PMW)
@@ -31,35 +62,64 @@ const int learntDefault = 45;                    //default value for learnt
 //servo motor configurations
 Servo servos[5];     //[0]:base servo (horizental move), [1]:first node (vertical move), [2]:second node (verrical move),
 //[3]: claw tilt (horizental move) [4]:claw servo (horizental move)
-int servoPins[] = {11, 10, 9, 6, 5};
-
+int servoPins[] = {11, 10, 9, 8, 7};
+Servo servo;
 const double ratioAngles = 0.67;        //the min/max angle ratio between the servos 2 and 3 (should be customized)
+//variables to hold values
+int yaw = 0;
+int pitch = 0;
+int roll = 0;
 
 //The structure for the final outputs of gyroscope sensor
 struct gyroSensor {
-  float roll;
-  float pitchOne;
-  float pitchTwo;
-  float yaw;
+  int roll;
+  int pitchOne;
+  int pitchTwo;
+  int yaw;
 
 };
 //creating a structure for the gyroscope sensor
 gyroSensor gyroValues;
 
 //gyroscope sensor needed variables
-const int MPU = 0x68;                                           // MPU6050 I2C address
-float AccX, AccY, AccZ;                                         //to record the accelaration values from sensor
-float GyroX, GyroY, GyroZ;                                      //to record the gyroscope values from sensor
-float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ; //to record the calculated angles from the sensor
-float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ; //to record the IMU errors
-float elapsedTime, currentTime, previousTime;                   //to record times between happenings
-float yaw, roll, pitch;                                         //the final outputs before mapping for servos
-int errorCounter = 0;                                           //a variable that holds the number of test runs
+
+MPU6050 mpu;// class default I2C address is 0x68
+#define OUTPUT_READABLE_YAWPITCHROLL
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
 
 
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
 void setup() {
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  Wire.begin();
+  TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+#elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+  Fastwire::setup(400, true);
+#endif
   //Starting serial communications
-  Serial.begin(19200);
+  Serial.begin(115200);
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
   Serial.println(" Starting...");
 
   //setting the I/O pin mode
@@ -72,19 +132,10 @@ void setup() {
   //color coding the mode, starting white
   rgbLed('w');
   //Attaching servo pin
+  servo.attach(12);
   for (int i = 0; i < sizeof(servoPins); i++) {
     servos[i].attach(servoPins[i]);
   }
-
-  //Setting up the gyroscope sensor
-  Wire.begin();                      // Initialize comunication
-  Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-  Wire.write(0x6B);                  // Talk to the register 6B
-  Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-  Wire.endTransmission(true);        //end the transmission
-  //this function get the IMU error values for module
-  calculate_IMU_error();
-  delay(20);
 
   //intitial values for sensor values
   gyroValues.yaw = 90;       //base servo
@@ -99,12 +150,66 @@ void setup() {
       learnt[i][j] = learntDefault;
     }
   }
-  attachInterrupt(1, ISR0, RISING); //attaching an interrupt for changing mode
+  attachInterrupt(1, ISR1, RISING); //attaching an interrupt for changing mode
+
+
+  //Setting up the gyroscope sensor
+// initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  mpu.initialize();
+
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+   /*
+  // wait for ready
+  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+  while (Serial.available() && Serial.read()); // empty buffer
+  while (!Serial.available());                 // wait for data
+  while (Serial.available() && Serial.read()); // empty buffer again
+   */
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
+
+    // enable Arduino interrupt detection
+    Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+    // ERROR! 
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
+  }
+
+
   Serial.println("Ready!");
   speakerToneOne();                //indicating the start of program
 
 }//end of void setup
-
 
 void loop() {
   //------------------------------------------------------------------------
@@ -172,9 +277,18 @@ void loop() {
 }//end of void loop
 
 
-//intterupt Zero (ISR0)
-void ISR0() {
-  detachInterrupt(0); //turning off the interrupt
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+//mpu int intrupt function
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+//intterupt One (ISR1)
+void ISR1() {
+  detachInterrupt(1); //turning off the interrupt
   if (modeMan) {
     modeMan = false;         //setting the mode from manual to learning
     modeAuto = false;     //set the mode auto off
@@ -185,8 +299,12 @@ void ISR0() {
     Serial.println("\n################\nSet mode to manual\n################");
   }
   delay(300);
-  attachInterrupt(0, ISR0, RISING);
+  attachInterrupt(1, ISR1, RISING);
 }
+
+// ================================================================
+// ===                    PRINT  METHODS                        ===
+// ================================================================
 
 //print all the outputs from the sensors
 void printData() {
@@ -215,6 +333,10 @@ void autoPrint(int number) {
   Serial.println("\n");
 }
 
+// ================================================================
+// ===                    SENSOR  METHODS                       ===
+// ================================================================
+
 //flex sensor input method
 int flexSensor() {
   int flexInput = analogRead(flexPin);
@@ -222,66 +344,83 @@ int flexSensor() {
 }
 //Gyroscope Sensor input method
 void gyroSensor() {
-  //Read acceleromter data
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  //For a range of +-2g, we need to divide the raw values by 16384, according to the datasheet
-  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
-  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
-  // Calculating Roll and Pitch from the accelerometer data
-  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - 0.58; // AccErrorX ~(0.58) See the calculate_IMU_error()custom function for more details
-  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) + 1.58; // AccErrorY ~(-1.58)
 
-  //Read gyroscope data
-  previousTime = currentTime;        // Previous time is stored before the actual time read
-  currentTime = millis();            // Current time actual time read
-  elapsedTime = (currentTime - previousTime) / 1000; // Divide by 1000 to get seconds
-  Wire.beginTransmission(MPU);
-  Wire.write(0x43); // Gyro data first register address 0x43
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 4 registers total, each axis value is stored in 2 registers
-  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0; // For a 250deg/s range we have to divide first the raw value by 131.0, according to the datasheet
-  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
-  // Correcting the outputs with the calculated error values
-  GyroX = GyroX - GyroErrorX; // GyroErrorX
-  GyroY = GyroY - GyroErrorY; // GyroErrorY
-  GyroZ = GyroZ - GyroErrorX; // GyroErrorZ
 
-  // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
-  gyroAngleX = gyroAngleX + GyroX * elapsedTime; // deg/s * s = deg
-  gyroAngleY = gyroAngleY + GyroY * elapsedTime;
-  yaw =  yaw + GyroZ * elapsedTime;
+  if (!dmpReady) return;
+  // wait for MPU interrupt or extra packet(s) available
+  while (!mpuInterrupt && fifoCount < packetSize) {
 
-  // Complementary filter - combine acceleromter and gyro angle values
-  roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-  pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
+  }
 
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = mpu.getFIFOCount();
+
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    // reset so we can continue cleanly
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+
+    // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+    // read a packet from FIFO
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+
+#ifdef OUTPUT_READABLE_YAWPITCHROLL
+    // display Euler angles in degrees
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180 / M_PI);
+    Serial.print("\t");
+    Serial.println(ypr[2] * 180 / M_PI);
+#endif
+  }
+
+
+  yaw = ypr[0] * 180 / M_PI;
+  pitch = ypr[1] * 180 / M_PI;
+  roll = ypr[2] * 180 / M_PI;
   //mapping the values for servo motors
-  gyroValues.roll  =  map (roll,  -100, 100, 0, 180);
-  gyroValues.yaw   =  map (yaw,   -100, 100, 0, 180);
-  if (pitch <= 50 && pitch >= -50) {
-    gyroValues.pitchOne = map (pitch, -50, 50, 0, 180);
+  gyroValues.roll  =  map (roll,  -80, 80, 0, 180);
+  gyroValues.yaw   =  (180-map (yaw,   -180, 180, 0, 180));
+ gyroValues.pitchOne =(180- map (pitch, -80, 80, 0, 180));
+  /*
+  if (pitch <= 40 && pitch >= -40) {
+    gyroValues.pitchOne =(180- map (pitch, -40, 40, 0, 180));
   } else {
     int temp = pitch;
-    if (temp > 50) {
-      temp -= 50;
+    if (temp > 40) {
+      temp -= 40;
     } else {
-      temp += 50;
+      temp += 40;
     }
     //0 to 60 because the second angle doesn't need to open all the way
-    int pitchTwoTemp = map (temp, -50, 50, 0, 60);
-    gyroValues.pitchTwo = ratio(pitchTwoTemp,gyroValues.pitchOne);
+    int pitchTwoTemp = map (temp, -40, 40, 0, 60);
+    gyroValues.pitchTwo = ratio(pitchTwoTemp, gyroValues.pitchOne);
   }
+
+  */
   delay(50);
 }
 
 //this function will make sure of the ratio of angles between servo 2 and 3
-int ratio(int inner, int outer){ //this function should be customized based on your structure
-  return int((outer*ratioAngles)+inner);            //0.67 is the ratio of angles between the two motors
+int ratio(int inner, int outer) { //this function should be customized based on your structure
+  return int((outer * ratioAngles) + inner);        //0.67 is the ratio of angles between the two motors
 }
 
 //this method will write the final output from the sensors to an array
@@ -293,9 +432,14 @@ void learnValues(int number) { //the input is the counter from the loop
   learnt[4][number] = flexValue;            //claw
 }
 
+// ================================================================
+// ===                     MOTOR  METHODS                       ===
+// ================================================================
+
 //set the values from the sensors into the servos
 void setServos() {
-  servos[0].write(gyroValues.yaw);       //base servo
+  servo.write(gyroValues.yaw);
+  servos[5].write(gyroValues.yaw);       //base servo
   servos[1].write(gyroValues.pitchOne);  //node one
   servos[2].write(gyroValues.pitchTwo);  //node two
   servos[3].write(gyroValues.roll);      //claw tilt
@@ -303,56 +447,18 @@ void setServos() {
 }
 //this method will write the learnt values to servo motors
 void autoSetServos(int number) {
+    servo.write(learnt[0][number]);
   servos[0].write(learnt[0][number]);       //base servo
   servos[1].write(learnt[1][number]);  //node one
   servos[2].write(learnt[2][number]);  //node two
   servos[3].write(learnt[3][number]);      //claw tilt
   servos[4].write(learnt[4][number]);            //claw
 }
-//this method will calculate the error in the readings from the gyroscope sensor.
-void calculate_IMU_error() {
-  // We can call this funtion in the setup section to calculate the accelerometer and gyro data error.
-  // Note that we should place the IMU flat in order to get the proper values, so that we then can the correct values.
-  errorCounter = 0;
-  // Read accelerometer values 300 times
-  while (errorCounter < 300) {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x3B);
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU, 6, true);
-    AccX = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    AccY = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0 ;
-    // Sum all readings
-    AccErrorX = AccErrorX + ((atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI));
-    AccErrorY = AccErrorY + ((atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI));
-    errorCounter++;
-  }
-  //Divide the sum by 300 to get the error value
-  AccErrorX = AccErrorX / 300;
-  AccErrorY = AccErrorY / 300;
-  errorCounter = 0;
-  // Read gyro values 300 times
-  while (errorCounter < 300) {
-    Wire.beginTransmission(MPU);
-    Wire.write(0x43);
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU, 6, true);
-    GyroX = Wire.read() << 8 | Wire.read();
-    GyroY = Wire.read() << 8 | Wire.read();
-    GyroZ = Wire.read() << 8 | Wire.read();
-    // Sum all readings
-    GyroErrorX = GyroErrorX + (GyroX / 131.0);
-    GyroErrorY = GyroErrorY + (GyroY / 131.0);
-    GyroErrorZ = GyroErrorZ + (GyroZ / 131.0);
-    errorCounter++;
-  }
-  //Divide the sum by 200 to get the error value
-  GyroErrorX = GyroErrorX / 300;
-  GyroErrorY = GyroErrorY / 300;
-  GyroErrorZ = GyroErrorZ / 300;
-}
 
+// ================================================================
+// ===                    OTHER  METHODS                       ===
+// ================================================================
+ 
 //a method for the rgb led color settings
 void rgbLed(char color) {
   digitalWrite(ledRedPin, LOW);
