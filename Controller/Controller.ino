4301aee975749e904fb19d15c328a78ae416222d
Milad Mobini <miladmobini.ca@gmail.com>
2021-05-07 13:39:41 -0400

    controller code complrtr[2~eted

--- Controller/Controller.ino
+++ Controller/Controller.ino
@@ -25,16 +25,22 @@ VarSpeedServo servos[5];
 int servoValues[5];
 //servo Speed
 int servoSpeed = 60;
+//reset servo value
+int resetServo[5];
+
 
 //threash holds
-int increaseThree = 900;
-int increaseTwo = 600;
-int increaseOne = 300;
-int middle = 100;
-int decreaseOne = -400;
-int decreaseTwo = -600;
-int decreaseThree = -900;
-
+int increaseThree = 852;
+int increaseTwo = 682;
+int increaseOne = 550;
+int middle = 512;
+int decreaseOne = 470;
+int decreaseTwo = 305;
+int decreaseThree = 130;
+//change values
+int changeValueOne = 2;
+int changeValueTwo = 5;
+int changeValueThree = 10;
 //pauses
 int eachRoundPause = 150;
 int shortPause = 50;
@@ -42,122 +48,142 @@ int longPause = 250;
 
 void setup()
 {
-    //starting serial communication
-    Serial.begin(9600);
-    Serial.println(" Starting...");
-    //attaching servo pins
-    servos[1].attach(S1P);
-    servos[2].attach(S2P);
-    servos[3].attach(S3P);
-    servos[4].attach(S4P);
-    servos[5].attach(S5P);
-    //giving intitial values
-    for (int i = 0; i < 5; i++)
-    {
-        servoValues[i] = 90;
-    }
+  //starting serial communication
+  Serial.begin(9600);
+  Serial.println(" Starting...");
+  //attaching servo pins
+  servos[0].attach(S1P);
+  servos[1].attach(S2P);
+  servos[2].attach(S3P);
+  servos[3].attach(S4P);
+  servos[4].attach(S5P);
+  //rest values
+  resetServo[0] = 90;
+  resetServo[1] = 160;
+  resetServo[2] = 12  0;
+  resetServo[3] = 90 ;
+  resetServo[4] = 170 ;
+  //giving intitial values
+  resetValues();
+  //buttons/switches
+  pinMode(LSP, INPUT_PULLUP);
+  pinMode(RSP, INPUT_PULLUP);
+  digitalWrite(LSP, HIGH);
+  digitalWrite(RSP, HIGH);
 
-    Serial.println("Ready!");
+  Serial.println("Ready!");
 }
 
 void loop()
 {
-    //print data
-    printData();
-    //getting and setting values
-    readShockToServoValue(analogRead(LXP), servoValues[0]);
-    readShockToServoValue(analogRead(LYP), servoValues[1]);
-    readShockToServoValue(analogRead(RXP), servoValues[2]);
-    readShockToServoValue(analogRead(RYP), servoValues[3]);
-    // readShockToServoValue(analogRead(LXP),servoValues[4]);
-    //writting to servos
-    writeServos();
-    delay(eachRoundPause);
+  //print data
+  printData();
+  //getting and setting values
+  LX = analogRead(LXP);
+  LY = analogRead(LYP);
+  RX = analogRead(RXP);
+  RY = analogRead(RYP);
+
+  readShockToServoValue(LX, 2);
+  readShockToServoValue(LY, 3);
+  readShockToServoValue(RX, 1);
+  readShockToServoValue(RY, 0);
+
+  if (digitalRead(LSP) == 0 ) {
+    toggleHook(4);
+  }
+  if (digitalRead(RSP) == 0 ) {
+    resetValues();
+  }
+  //writting to servos
+  writeServos();
+  delay(eachRoundPause);
 }
 
 /**
- * Write servoValues to servos
- */
-void writeServos(){
-    for (int i = 0; i < 5; i++)
-    {
-        servos[i].write(servoValues[i], servoSpeed);
-    }
-}
-    /**
- * Toggle hook switch value
- */
-    boolean toggleHook(int shockValue, int servoValue)
-{
-    if (servoValue == 170)
-    {
-        servoValue = 30;
-    }
-    else
-    {
-        servoValue = 170;
-    }
-    return true;
+   Write servoValues to servos
+*/
+void writeServos() {
+  for (int i = 0; i < 5; i++)
+  {
+    servos[i].write(servoValues[i], servoSpeed);
+  }
 }
 /**
- * get the read value from shock pin and pass it through conditions and write result to servoValue
- * 
- */
-boolean readShockToServoValue(int shockValue, int servoValue)
+   reset all the servo values to 90
+*/
+void resetValues() {
+  for (int i = 0; i < 5; i++)
+  {
+    servoValues[i] = resetServo[i];
+  }
+}
+/**
+  Toggle hook switch value
+*/
+boolean toggleHook(int number)
 {
-    if (shockValue >= increaseThree && servoValue < 177)
-    {
-        servoValue += 3;
-    }
-    else if (shockValue >= increaseTwo && servoValue < 178)
-    {
-        servoValue += 2;
-    }
-    else if (shockValue >= increaseOne && servoValue < 179)
-    {
-        servoValue += 1;
-    }
-    else if (shockValue >= middle)
-    {
-        //nothing
-    }
-    else if (shockValue >= decreaseThree && servoValue > 3)
-    {
-        servoValue -= 3;
-    }
-    else if (shockValue >= decreaseTwo && servoValue > 2)
-    {
-        servoValue -= 2;
-    }
-    else if (shockValue >= decreaseOne && servoValue > 1)
-    {
-        servoValue -= 1;
-    }
-    else
-    {
-        //nothing
-    }
-    return true;
+  if (servoValues[number] == 170)
+  {
+    servoValues[number] = 30;
+  }
+  else
+  {
+    servoValues[number] = 170;
+  }
+  return true;
+}
+/**
+   get the read value from shock pin and pass it through conditions and write result to servoValue number
+
+*/
+boolean readShockToServoValue(int shockValue, int number)
+{
+  if (shockValue >= increaseThree && servoValues[number] < (180 - changeValueOne))
+  {
+    servoValues[number] += changeValueThree;
+  }
+  else if (shockValue >= increaseTwo && servoValues[number] < (180 - changeValueTwo))
+  {
+    servoValues[number] += changeValueTwo;
+  }
+  else if (shockValue >= increaseOne && servoValues[number] < (180 - changeValueThree))
+  {
+    servoValues[number] += changeValueOne;
+  }
+  else if (shockValue <= decreaseThree && servoValues[number] > changeValueThree)
+  {
+    servoValues[number] -= changeValueThree;
+  } else if (shockValue <= decreaseTwo && servoValues[number] > changeValueTwo)
+  {
+    servoValues[number] -= changeValueTwo;
+  }
+  else if (shockValue <= decreaseOne && servoValues[number] > changeValueOne)
+  {
+    servoValues[number] -= changeValueOne;
+  }
+
+  return true;
 }
 
 void printData()
 {
-    Serial.print("LX");
-    Serial.print(LX);
-    Serial.print("  LY:");
-    Serial.print(LY);
-    Serial.print("  RX:");
-    Serial.print(RX);
-    Serial.print("  RY:");
-    Serial.print(RY);
-    Serial.print("  S1:");
-    Serial.print(servoValues[0]);
-    Serial.print("  S2:");
-    Serial.print(servoValues[1]);
-    Serial.print("  S3:");
-    Serial.print(servoValues[2]);
-    Serial.print("  S4:");
-    Serial.print(servoValues[3]);
-    Serial.print("  S5:");
-    Serial.println(servoValues[4]);
+  Serial.print("LX");
+  Serial.print(LX);
+  Serial.print("  LY:");
+  Serial.print(LY);
+  Serial.print("  RX:");
+  Serial.print(RX);
+  Serial.print("  RY:");
+  Serial.print(RY);
+  Serial.print("  S1:");
+  Serial.print(servoValues[0]);
+  Serial.print("  S2:");
+  Serial.print(servoValues[1]);
+  Serial.print("  S3:");
+  Serial.print(servoValues[2]);
+  Serial.print("  S4:");
+  Serial.print(servoValues[3]);
+  Serial.print("  S5:");
+  Serial.println(servoValues[4]);
 }
